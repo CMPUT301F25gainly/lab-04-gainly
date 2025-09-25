ccfa021981a34a27374ed2604e97f119423789b9
Zhouyi Yu <zhouyi1@ualberta.ca>
2025-09-24 19:18:05 -0600

    Zhouyi adds Pentagon in his own Branch now, Hooray

---
+++ app/src/main/java/com/example/lab_04_gainly/Pentagon.java
@@ -0,0 +1,12 @@
+package com.example.lab_04_gainly;
+
+public class Pentagon extends Shape{
+    private int sideLength;
+    public Pentagon(int sideLength){
+        this.sideLength = sideLength;
+    }
+
+    public int circumference(){
+        return sideLength*5;
+    }
+}
