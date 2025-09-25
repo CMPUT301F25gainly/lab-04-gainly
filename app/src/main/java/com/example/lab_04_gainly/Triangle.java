ca3a624544f9e347b32f84dd566777f7e1328cfd
Ilhamoddin Arbabzada <112998037+ilhamoddin@users.noreply.github.com>
2025-09-25 16:06:15 -0600

    Ilham-add triangle class

---
+++ app/src/main/java/com/example/lab_04_gainly/Triangle.java
@@ -0,0 +1,10 @@
+package com.example.lab_04_gainly;
+
+public class Triangle extends Shape{
+
+    public Triangle() {super (1, 1);}
+
+    public Triangle(int x, int y) {
+        super(x,y);
+    }
+}
