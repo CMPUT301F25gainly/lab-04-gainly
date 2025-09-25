af0d5f01d1b15167d3dbab55f83bd2c5461599c6
Jason Chang <zechen6@ualberta.com>
2025-09-24 20:01:49 -0600

    i must scream

---
+++ app/src/main/java/com/example/lab_04_gainly/Star.java
@@ -0,0 +1,13 @@
+package com.example.lab_04_gainly;
+
+public class Star extends Shape {
+
+    public Star() {
+        super(1, 1);
+    }
+
+    public Star(int x, int y) {
+        super(x, y);
+    }
+}
+
